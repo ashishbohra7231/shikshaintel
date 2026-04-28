8a4dacb16a4e6d174dd6cd6e3eb62816221bbe27
Ashish Bohra <ashishbohra1208@gmail.com>
2026-04-28 18:55:10 +0530

    test

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,22 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+// next.config.js
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "shikshaintel.in",
+          },
+        ],
+        destination: "https://www.shikshaintel.in/:path*",
+        permanent: true,
+      },
+    ];
+  },
+};
 export default nextConfig;
